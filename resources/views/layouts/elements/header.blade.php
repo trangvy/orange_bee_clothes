2b042c37e1dea8b30d0ffeae43cdca08e64bf923
vytrang <clara@pstworld.com>
2019-07-22 15:45:35 +0700

    Create layouts

---
+++ resources/views/layouts/elements/header.blade.php
@@ -0,0 +1,295 @@
+<!--Start Header area  -->
+<header class="header" id="home">
+   <div class="header_area">
+       <div class="header_top_area">
+           <div class="container">
+               <div class="row">
+                   <div class="col-lg-2 col-md-2 hidden-sm col-xs-12">
+                       <div class="wel_msg">
+                           <p>Default welcome msg!</p>
+                       </div>
+                   </div>
+                   <div class="col-lg-10 col-md-10 col-sm-12 hidden-xs">
+                       <div class="header_top_right">
+                           <ul>
+                               <li><a href="#">USD <i class="fa fa-angle-down"></i></a>
+                                   <ul class="currency_submenu">
+                                       <li><a href="#">SGD</a></li>
+                                       <li><a href="#">BDT</a></li>
+                                       <li><a href="#">EUR</a></li>
+                                   </ul>
+                               </li>
+                               <li><a href="#"><img src="{{ asset('images/english.jpg') }}" alt=""> English <i class="fa fa-angle-down"></i></a>
+                                   <ul class="langulage_submenu">
+                                       <li><a href="#"><img src="{{ asset('images/english.jpg') }}" alt=""> Italiano</a></li>
+                                       <li><a href="#"><img src="{{ asset('images/nl_nl.gif') }}" alt=""> Nederlands</a></li>
+                                       <li><a href="#"><img src="{{ asset('images/de_de.gif') }}" alt=""> Deutsch</a></li>
+                                       <li><a href="#"><img src="{{ asset('images/he_il.gif') }}" alt=""> עברית</a></li>
+                                   </ul>
+                               </li>
+                               <li><a href="#"><i class="fa fa-user"></i> My Account</a></li>
+                               <li><a href="#"><i class="fa fa-heart"></i> Wise List(0)</a></li>
+                               <li><a href="#"><i class="fa fa-shopping-cart"></i> Shopping Cart</a></li>
+                               <li><a href="#"><i class="fa fa-share-square-o"></i> Checkout</a></li>
+                           </ul>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+       <div class="main_header">
+           <div class="container">
+               <div class="row">
+                   <div class="col-lg-1 col-md-1 col-sm-2 col-xs-12">
+                       <div class="logo">
+                           <a href="#"><img src="{{ asset('images/logo.png') }}" alt="Deli"></a>
+                       </div>
+                   </div>
+                   <div class="col-lg-6 col-md-6 col-sm-10">
+                       <div class="mainmenu nav navbar-collapse collapse navbar-right">
+                            <nav>
+                                <ul class="navbar-nav" id="nav">
+                                    <li class="drop active"><a href="#">home</a>
+                                        <ul class="sub-menu">
+                                            <li class="title"><a href="#">HOMEPAGES</a></li>
+                                            <li><a href="#">Homepage Version 1</a></li>
+                                            <li><a href="#">Homepage Version 2</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="men"><a href="#">men</a>
+                                        <div class="mega_menu">
+                                            <span>
+                                                <a href="#" class="title">SHOES</a>
+                                                <a href="#">Sports</a>
+                                                <a href="#">Run</a>
+                                                <a href="#">Sandals</a>
+                                                <a href="#">Books</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">BAGS</a>
+                                                <a href="#">Blazers</a>
+                                                <a href="#">Table</a>
+                                                <a href="#">Coats</a>
+                                                <a href="#">Kids</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">CLOTH</a>
+                                                <a href="#">T-shirts</a>
+                                                <a href="#">Coats</a>
+                                                <a href="#">Jackets</a>
+                                                <a href="#">Jeans</a>
+                                            </span>
+                                            <span>
+                                                <a class="menu_img" href="#"><img src="{{ asset('images/sale2.jpg') }}" alt="sale"></a>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li class="women"><a href="#">women</a>
+                                        <div class="mega_menu">
+                                            <span>
+                                                <a href="#" class="title">DRESSES</a>
+                                                <a href="#">Cocktail</a>
+                                                <a href="#">Formal</a>
+                                                <a href="#">Evening</a>
+                                                <a href="#">Wedding</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">SHOES</a>
+                                                <a href="#">Run</a>
+                                                <a href="#">Sports</a>
+                                                <a href="#">Sandals</a>
+                                                <a href="#">Heels</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">BAGS</a>
+                                                <a href="#">Blazers</a>
+                                                <a href="#">Coats</a>
+                                                <a href="#">Table</a>
+                                                <a href="#">Kids</a>
+                                            </span>
+                                            <span>
+                                                <a class="menu_img" href="#"><img src="{{ asset('images/sale3.jpg') }}" alt="sale"></a>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li class="accesories"><a href="#">accesories</a>
+                                        <div class="mega_menu">
+                                            <span>
+                                                <a href="#" class="title">BAGS</a>
+                                                <a href="#">Suitcase</a>
+                                                <a href="#">Wallet</a>
+                                                <a href="#">Handbag</a>
+                                                <a href="#">Lady's Bag</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">GLASSES</a>
+                                                <a href="#">Formal</a>
+                                                <a href="#">Sunglasses</a>
+                                                <a href="#">Fancy</a>
+                                                <a href="#">Classy</a>
+                                            </span>
+                                            <span>
+                                                <a href="#" class="title">WATCHES</a>
+                                                <a href="#">Gent'S</a>
+                                                <a href="#">Lady'S</a>
+                                                <a href="#">Kid's</a>
+                                                <a href="#">Wedding</a>
+                                            </span>
+                                            <span>
+                                                <a class="menu_img" href="#"><img src="{{ asset('images/bag3.jpg') }}" alt="bag"></a>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li class="pages"><a href="#">pages</a>
+                                        <div class="mega_menu">
+                                            <span>
+                                                <a href="#">blog</a>
+                                                <a href="#">About Us</a>
+                                                <a href="#">blog details</a>
+                                                <a href="#">Product Details</a>
+                                            </span>
+                                            <span>
+                                                <a href="#">Shop</a>
+                                                <a href="#">Checkout</a>
+                                                <a href="#">Shop Leftsidebar</a>
+                                                <a href="#">Shop Rightsidebar</a>
+                                            </span>
+                                            <span>
+                                                <a href="#">Cart</a>
+                                                <a href="#">Contact</a>
+                                                <a href="#">404 Error</a>
+                                            </span>
+                                        </div>
+                                    </li>
+                                    <li><a href="#">watches</a></li>
+                                </ul>
+                            </nav>
+                       </div>
+                   </div>
+                   <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                       <div class="category-search-cart clearfix">
+                            <div class="category">
+                                <a href="#">ALL Category <i class="fa fa-chevron-down"></i></a>
+                                <ul class="category_submenu">
+                                    <li><a href="#">Women</a></li>
+                                    <li><a href="#">Tops</a></li>
+                                    <li><a href="#">T-shirt</a></li>
+                                    <li><a href="#">Shoe</a></li>
+                                    <li><a href="#">Bags</a></li>
+                                </ul>
+                            </div>
+                           <div class="search">
+                               <form>
+                                    <input type="text" name="search" id="search" placeholder="Search here...">
+                                    <input type="submit" value="Search">
+                               </form>
+                           </div>
+                           <div class="cart-total">
+                                <ul>
+                                    <li>
+                                       <div class="shopping-cart">
+                                           <a href="#"><i class="fa fa-shopping-cart"></i>
+                                           <span class="cart-count">2</span></a>
+                                       </div>
+                                        <div class="mini-cart-content">
+                                            <div class="cart-img-details">
+                                                <div class="cart-img-photo">
+                                                    <a href="#"><img src="{{ asset('images/sale2.jpg') }}" alt="sale"></a>
+                                                    <span class="quantity">1</span>
+                                                </div>
+                                                <div class="cart-img-contaent">
+                                                    <a href="#"><p>Vestibulum suscipit</p></a>
+                                                    <span>£165.00</span>
+                                                </div>
+                                                <div class="pro-del"><a href="#"><i class="fa fa-times-circle"></i></a></div>
+                                            </div>
+                                            <div class="clear"></div>
+                                            <div class="cart-img-details">
+                                                <div class="cart-img-photo">
+                                                    <a href="#"><img src="{{ asset('images/sale4.jpg') }}" alt="sale"></a>
+                                                    <span class="quantity">1</span>
+                                                </div>
+                                                <div class="cart-img-contaent">
+                                                    <a href="#"><p>Donec sodales mauris in</p></a>
+                                                    <span>£50.00</span>
+                                                </div>
+                                                <div class="pro-del"><a href="#"><i class="fa fa-times-circle"></i></a></div>
+                                            </div>
+                                            <div class="cart-inner-bottom">
+                                                <p class="total">Subtotal: <span class="amount">£215.00</span></p>
+                                                <div class="clear"></div>
+                                                <p class="cart-button-top"><a href="#">Checkout</a></p>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                       </div>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   <!-- mobile-menu-area start -->
+    <div class="mobile-menu-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="mobile-menu">
+                        <div class="logo_menu">
+                            <a href="#"><img src="{{ asset('images/logo.png') }}" alt=""></a>
+                        </div>
+                        <nav id="dropdown" style="display: block;">
+                            <ul>
+                                <li class="drop active"><a href="#">home</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Homepage Version 1</a></li>
+                                        <li><a href="#">Homepage Version 2</a></li>
+                                    </ul>
+                                </li>
+                                <li class="drop active"><a href="#">men</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Shoes</a></li>
+                                        <li><a href="#">Bags</a></li>
+                                        <li><a href="#">Cloths</a></li>
+                                    </ul>
+                                </li>
+                                <li class="drop active"><a href="#">women</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">dresses</a></li>
+                                        <li><a href="#">Shoes</a></li>
+                                        <li><a href="#">Bags</a></li>
+                                    </ul>
+                                </li>
+                                <li class="drop active"><a href="#">Accesories</a>
+                                    <ul class="sub-menu">
+                                        <li><a href="#">Bags</a></li>
+                                        <li><a href="#">Glasses</a></li>
+                                        <li><a href="#">Watches</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Pages</a>
+                                    <ul>
+                                        <li><a href="#">Blog</a></li>
+                                        <li><a href="#">About us</a></li>
+                                        <li><a href="#">Blog Details</a></li>
+                                        <li><a href="#">Product Details</a></li>
+                                        <li><a href="#">Shop</a></li>
+                                        <li><a href="#">Checkout</a></li>
+                                        <li><a href="#">Shop Leftsidebar</a></li>
+                                        <li><a href="#">Shop Rightsidebar</a></li>
+                                        <li><a href="#">Cart</a></li>
+                                        <li><a href="#">404 Error</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- mobile-menu-area end -->
+</header>
+<!--End Header area  -->
